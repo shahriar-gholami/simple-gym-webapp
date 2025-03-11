5e6b48d3daba6c3446a0f169471bc3ac1a000d60
nasrin hosseinzadeh <120596560+shahriar-gholami@users.noreply.github.com>
2025-03-11 23:35:35 +0330

    instructor role added

--- core/shop/models.py
+++ core/shop/models.py
@@ -39,55 +39,36 @@ class Customer(models.Model):
 	def __str__(self):
 		return f'{self.full_name}'
 
+class Instructor(models.Model):
+	full_name = models.CharField(max_length=250)
+	phone_number = models.CharField(max_length=250)
+	national_code = models.CharField(max_length=250)
+	courses = models.ManyToManyField(CourseTitle, blank=True)
+	created_date = models.DateTimeField(auto_now_add=True, null=True)
+
+	@property
+	def register_date(self):
+		return JalaliDatetime(self.create_date).strftime('%Y/%m/%d')
+
 class ReservedCourse(models.Model):
 	title = models.ForeignKey(CourseTitle, on_delete=models.CASCADE)
 	customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 	num_of_sessions = models.IntegerField(default=0)
 	cost_paid = models.IntegerField(default=0)
 	register_date = models.DateTimeField(auto_now_add=True)
-	# register_date = models.CharField(max_length=250)
+	Instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE, null=True, blank=True)
 
 	@property
 	def shamsi_register_date(self):
 		return JalaliDatetime(self.register_date).strftime('%Y/%m/%d')
 
-	# def get_register_day(self):
-	# 	from_time_words=self.register_date.split()
-	# 	if from_time_words[1] == 'شنبه':
-	# 		from_time_day = int(from_time_words[2])
-	# 	else:
-	# 		from_time_day = int(from_time_words[1])
-	# 	return from_time_day
-
-	# def get_register_month(self):
-	# 	months_dict = {
-	# 					'فروردین': 1,
-	# 					'اردیبهشت': 2,
-	# 					'خرداد': 3,
-	# 					'تیر': 4,
-	# 					'مرداد': 5,
-	# 					'شهریور': 6,
-	# 					'مهر': 7,
-	# 					'آبان': 8,
-	# 					'آذر': 9,
-	# 					'دی': 10,
-	# 					'بهمن': 11,
-	# 					'اسفند': 12
-	# 				}
-	# 	register_time_words=self.register_date.split()
-	# 	if register_time_words[2] in months_dict:
-	# 		register_time_month = months_dict[register_time_words[2]]
-	# 	elif register_time_words[3] in months_dict:
-	# 		register_time_month = months_dict[register_time_words[3]]
-	# 	return register_time_month
-
-	# def get_register_year(self):
-	# 	register_time_words=self.register_date.split()
-	# 	if register_time_words[3].isdigit():
-	# 		register_time_year = int(register_time_words[3])
-	# 	else:
-	# 		register_time_year = int(register_time_words[4])
-	# 	return register_time_year
-
 	def __str__(self):
 		return f'{self.title} - {self.customer.full_name}'
+
+
+
+
+
+
+
+
